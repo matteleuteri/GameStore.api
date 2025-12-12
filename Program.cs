0c102a634a0046773f677f953139e14e97519df3
DonnyWu <donny.wu4@gmail.com>
2025-12-11 23:55:28 -0500

    Feature--Added Additonal Sample data and  api to retrieve information

--- Program.cs
+++ Program.cs
@@ -3,14 +3,29 @@ using GameStore.api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
 List<GameDto> games = [
-    new(1, 
-    "Pokemon: Heartgold",
-    "Role-Playing",
-    69.99M,
-    new DateOnly(2010, 3, 14))
+    new(
+        1, 
+        "Pokemon: Heartgold",
+        "Role-Playing",
+        12.72M,
+        new DateOnly(2010, 3, 14)),
+    new (
+        2, 
+        "Minecraft", 
+        "Sandbox",
+        350M, 
+        new DateOnly(2009, 5, 17)),
+    new (
+        3, 
+        "Wii Sports Resort",
+        "Sports", 
+        33.14M ,
+        new DateOnly(2009, 6, 25))
 ];
+
+app.MapGet("games", () => games);
+
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
