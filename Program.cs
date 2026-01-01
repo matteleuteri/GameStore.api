5b76cc0cc3e51db311e51d4b2397b56682ca29b6
DonnyWu <donny.wu4@gmail.com>
2026-01-01 01:59:56 -0500

    Feature--Added Assembly for database mapping

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using GameStore.api.Endpoints;
+using GameStore.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
