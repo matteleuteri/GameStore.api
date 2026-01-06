f8d20a1d1ebfe7a95737b8c12e348d4f8a1256fc
DonnyWu <donny.wu4@gmail.com>
2026-01-06 01:49:33 -0500

    Feature--Added seeding to GameStoreContext.cs

--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -7,4 +7,17 @@ public class GameStoreContext(DbContextOptions <GameStoreContext> options): DbCo
     public DbSet<Game> Games => Set<Game>();
 
     public DbSet<Genre> Genres => Set<Genre>();
+
+    override protected void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Genre>().HasData(
+            new Genre { Id = 1, Name = "Fighting" },
+            new Genre { Id = 2, Name = "Roleplaying" },
+            new Genre { Id = 3, Name = "Sports" },
+            new Genre { Id = 4, Name = "Racing" },
+            new Genre { Id = 5, Name = "Kids and Family" }
+        );
+    }
 }
