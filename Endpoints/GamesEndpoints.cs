cd20c734cedddc79bdc1ac4f87f4f6453d8f62e5
DonnyWu <donny.wu4@gmail.com>
2026-01-14 23:51:22 -0500

    Feature--GameMapping for DTO and entity conversions

--- Endpoints/GamesEndpoints.cs
+++ Endpoints/GamesEndpoints.cs
@@ -1,6 +1,7 @@
 using GameStore.api.Dtos;
 using GameStore.api.Entities;
 using GameStore.Api.Data;
+using GameStore.api.Mapping;
 
 namespace GameStore.api.Endpoints;
 
@@ -51,27 +52,13 @@ public static class GameEndPoints
                 return Results.BadRequest("Name is required");
             }
 
-            Game game = new()
-            {
-                Name = newGame.Name,
-                Genre = dbContext.Genres.Find(newGame.GenreId),
-                GenreId = newGame.GenreId,
-                Price = newGame.Price,
-                ReleaseDate = newGame.ReleaseDate
-            };
-            
+            Game game = newGame.ToEntity();   
+            game.Genre = dbContext.Genres.Find(newGame.GenreId);     
+
             dbContext.Games.Add(game);
             dbContext.SaveChanges();
 
-            GameDto gameDto = new(
-                game.Id,
-                game.Name,
-                game.Genre!.Name ?? "Unknown",
-                game.Price,
-                game.ReleaseDate
-            );
-
-            return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, gameDto);
+            return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game.ToDto());
         });
 
         // PUT /games/1
